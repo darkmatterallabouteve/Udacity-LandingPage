29fd386acba7ab6a37eca3fff5216b6ae2432123
darkmatterallabouteve <darkmatterallabouteve@protonmail.com>
2021-11-16 14:49:44 -0500

    add readme template

--- js/app.js
+++ js/app.js
@@ -11,6 +11,16 @@
  * 
  * JS Standard: ESlint
  * 
+ * 
+ * Project Criteria
+ * Navigation is built dynamically as an unordered list.
+ * Start with empty ul and dynamically build navigation using
+ * Append, appendChild, and innerHTML.
+ * 
+ * It should be clear which section is being viewed while scrolling through the page. 
+ * 
+ * When clicking an item from the navigation menu, 
+ * the link should scroll to the appropriate section. 
 */
 
 /**
