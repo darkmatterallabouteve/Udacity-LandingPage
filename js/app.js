1afdfe96b34bfc354425553267785dbc437d8aac
darkmatterallabouteve <darkmatterallabouteve@protonmail.com>
2021-11-19 14:36:17 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,16 +16,12 @@
 const sectionList = document.getElementsByTagName("section");
 const navbarList = document.getElementById("navbar__list");
 
-let last_known_scroll_position = 0;
+let scroll_position = 0;
 
 // Scroll to anchor ID using scrollTO event
 function scrollToSection(evt) {
     if (evt.target.nodeName === 'A') {
         let sectionId = evt.target.getAttribute("data-sectionid");
-       
-        //console.log('A link was clicked with text ' 
-        //    + evt.target.textContent + ' sectionId=' + sectionId);
-
         let topCoord = window.scrollY + 
             document.querySelector('#' + sectionId).getBoundingClientRect().top;
 
@@ -38,15 +34,14 @@ function scrollToSection(evt) {
     }
 }
 
-// Build the nav-bar - Navigation is built dynamically as an unordered list.
+/* Build the nav-bar - Navigation is built dynamically as 
+*  an unordered list.
+*/
 for(const section of sectionList){
     let sectionName = section.getAttribute("data-nav");
     let sectionId = section.id;
-
-    //console.log(`sectionName.id: ${sectionName}`);
-    //console.log(`sectionId: ${sectionId}`);
-
     let newLi = document.createElement("li");
+
     newLi.setAttribute("class", "menu__link");
 
     let newNavLink = document.createElement("a");
@@ -60,7 +55,7 @@ for(const section of sectionList){
 *  section depending on what section is currently viewable near top of page
 */
 window.addEventListener('scroll', function(event) {
-    last_known_scroll_position = window.scrollX;
+    scroll_position = window.scrollX;
 
     const sectionList = document.getElementsByTagName("section");
     for(const section of sectionList){  
@@ -68,8 +63,8 @@ window.addEventListener('scroll', function(event) {
         let sectionName = section.getAttribute("data-nav");        
         let sectionTop = section.getBoundingClientRect().top;
     
-        if(last_known_scroll_position >= sectionTop - 120 
-            && last_known_scroll_position <= sectionTop + 120){
+        if(scroll_position >= sectionTop - 120 
+            && scroll_position <= sectionTop + 120){
             highlighterToggle(sectionName);
         }
     }
@@ -84,9 +79,9 @@ function highlighterToggle(highlightSectionName) {
     const navItemList = document.getElementsByClassName("menu__link");
     for(const navItem of navItemList){
         if(navItem.textContent == highlightSectionName){
-            navItem.setAttribute("class", "menu__link highlight");
+            navItem.classList.add("highlight");
         } else {
-            navItem.setAttribute("class", "menu__link");
+            navItem.classList.remove("highlight");
         }
     }
 
@@ -95,9 +90,9 @@ function highlighterToggle(highlightSectionName) {
     for(const section of sectionList){
         let sectionName = section.getAttribute("data-nav");
         if(sectionName == highlightSectionName){
-            section.setAttribute("class", "your-active-class");
+            section.classList.add("your-active-class");
         } else {
-            section.removeAttribute("class");
+            section.classList.remove("your-active-class");
         }
     }
 }
